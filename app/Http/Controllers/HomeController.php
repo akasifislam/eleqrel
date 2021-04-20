157c7fcf1d24b679abf4616c5e393a9b005e6dd6
akasifislam <aseasifislam@gmail.com>
2021-04-21 02:07:41 +0600

    one2one

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Nid;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+
+    public function userInfo()
+    {
+        // $user = User::find(Auth::user()->id);
+        // return view('welcome', compact('user'));
+
+        $nid = Nid::where('user_id', Auth::user()->id)->first();
+        return view('welcome', compact('nid'));
+    }
+}
